5d86e656b86b40f41b980753128f8c04e656606f
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-12-08 16:51:35 +0100

    EL now loads new gif on click

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const api = "https://api.giphy.com/v1/gifs/translate?api_key=1111111111&s=cats";
-const gif = document.querySelector(".gif");
+const api = "https://api.giphy.com/v1/gifs/translate?api_key=11111111111111&s=cats";
+const gif = document.getElementById("gif");
+const gifControls = document.querySelector(".gif-controls");
+let currUrl = null;
 
+// Returns GIF Promise
 const getGifData = async () => {
     const response = await fetch(api, {mode: 'cors'});
     const data = await response.json();
@@ -8,20 +11,30 @@ const getGifData = async () => {
     return data;
 }
 
-
+// Show GIF on first page load
 getGifData()
-    .then((data) => {
-        console.log(data.data.images.original.url);
+    .then((d) => {
+        gif.src = d.data.images.original.url;
+        console.log(d.data)
     })
     .catch((err) => {
-        console.log("Error: ", err);
-    })
+        console.log("Error setting Gif source: ", err);
+    }
+)
 
-// const gifData = getGifData();
-// fetch(api, {mode: 'cors'})
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(response) {
-//         gif.src = response.data.images.original.url;
-//     });
+// Change GIF source on button click
+gifControls.addEventListener("click", (e) => {
+    if (e.target.classList.contains("next-btn")){
+        console.log("next gif loading")
+        getGifData()
+            .then((d) => {
+                gif.src = d.data.images.original.url;
+                console.log(d.data)
+            })
+            .catch((err) => {
+                console.log("Error: ", err);
+            })
+    } else if (e.target.classList.contains("back-btn")){
+        console.log("previous gif loading");
+    }
+})
